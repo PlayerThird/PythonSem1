7ed0acef18a291e8cfad498c333643389804564a
PlayerThird030 <verevka.rud@gmail.com>
2023-03-07 21:13:06 +0300

    Семинар 6

--- Семинар6/sem46.py
+++ Семинар6/sem46.py
@@ -4,8 +4,19 @@
 # Программа должна вывести  все пары дружественных чисел, каждое из которых не превосходит k.
 # Пары необходимо выводить по одной в строке, разделяя пробелами.
 # Каждая пара должна быть выведена только один раз (перестановка чисел новую пару не дает).
-fir_chis = int(input("Введите первое число"))
-sec_chis = int(input("Введите второе число"))
-first_k = dict()
-second_m =dict()
+len_arr = int(input("Введите число"))
+my_dict = {}
+for i in range(1, len_arr):
+    my_dict[i] = my_dict.get(i,0)
+    for j in range(1, i//2+1):
+        if i % j == 0:
+            my_dict[i] = my_dict.get(i,0) + j
+
+print(my_dict)
+
+for key1, val1 in my_dict.items():
+    for key2, val2 in my_dict.items():
+        if key2>key1:
+            if key1!=key2 and val1 == key2 and val2 == key1:
+                print(f"{key1} {key2}")
 
